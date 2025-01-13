42ce6770441d1e8f7b5614453d486dc194d3f2eb
taichi3012 <68967462+taichi3012@users.noreply.github.com>
2025-01-13 14:17:29 +0900

    [Release]v1.2.0

--- src/main/java/com/github/taichi3012/controlime/ControlIME.java
+++ src/main/java/com/github/taichi3012/controlime/ControlIME.java
@@ -21,7 +21,7 @@ import static com.sun.jna.platform.win32.WinDef.*;
 public class ControlIME {
 
   public static final String MOD_ID = "controlime";
-  public static final String VERSION = "1.1.0";
+  public static final String VERSION = "1.2.0";
   public static Logger logger;
 
   @EventHandler
